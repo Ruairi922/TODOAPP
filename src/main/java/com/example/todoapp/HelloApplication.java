992cb5c660a1be317af95cd8b440854552d81699
Ruairi922 <136007712+Ruairi922@users.noreply.github.com>
2023-10-21 13:18:33 +0100

    Update HelloApplication.java
    make Screen size default 800x500. Make background of textfield area light blue

--- src/main/java/com/example/todoapp/HelloApplication.java
+++ src/main/java/com/example/todoapp/HelloApplication.java
@@ -12,9 +12,9 @@ public class HelloApplication  extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
-        root.setStyle("-fx-background-color: lightblue;"); // Use your desired color
+        root.setStyle("-fx-background-color: lightblue;"); // background colour light blue
         Scene scene = new Scene(root, 800, 500);
-        root.setStyle("-fx-background-color: lightblue;"); // Use your desired color
+        root.setStyle("-fx-background-color: lightblue;"); 
         primaryStage.setScene(scene);
         primaryStage.setTitle("To-Do App");
         primaryStage.show();
